ffbfa5bade3a1793af57e469e964bd4cadd0eaa7
WonderCsabo <kozakcsabi@gmail.com>
2015-06-06 14:51:19 +0200

    Add AA

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example;
 
 
+import android.app.Activity;
+
 import com.example.mylib.MyBean;
 import com.example.mylib.test.MyOtherBean;
 import com.example.mylib.test.MyThirdBean;
 
-public class MainActivity /*extends Activity*/ {
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.EActivity;
 
+@EActivity
+public class MainActivity extends Activity {
+
+    @Bean
     MyBean bean;
-
+    @Bean
     MyOtherBean otherBean;
-
+    @Bean
     MyThirdBean thirdBean;
 
 }
--- mylib/src/main/java/com/example/mylib/test/MyOtherBean.java
+++ mylib/src/main/java/com/example/mylib/test/MyOtherBean.java
@@ -1,9 +1,11 @@
 package com.example.mylib.test;
 
+import org.androidannotations.annotations.EBean;
+
 /**
  * Created by A605152 on 05/06/2015.
  */
-//@EBean
+@EBean
 public class MyOtherBean {
     public MyOtherBean(){}
 }
